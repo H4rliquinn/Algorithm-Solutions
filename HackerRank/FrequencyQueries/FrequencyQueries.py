beae2c526bae1721bb57994fee8e91299309d687
Jason Sonnichsen <Harliquinn@gmail.com>
2020-03-25 18:36:36 -0400

    Frequency Queries complete Forgot to check if frequency was over 0 in output

--- HackerRank/FrequencyQueries/FrequencyQueries.py
+++ HackerRank/FrequencyQueries/FrequencyQueries.py
@@ -22,7 +22,7 @@ def freqQuery(queries):
     # Data from the queries
     data={}
     # the values in this second dict will be the frequency of the frequencies
-    frequency={0:0}
+    frequency={}
     
     # Check for queries
     # if len(queries)<1:
@@ -36,46 +36,61 @@ def freqQuery(queries):
                 # increment the data if it exists in the first dict
                 data[q[1]]+=1
             else:
-                old_f=1
+                old_f=0
                 # init the data to a freq of 1 if it doesn't exist in the first dict
                 data[q[1]]=1
             new_f=data[q[1]]
+            
+            if old_f in frequency and frequency[old_f]>0:
+                # decrement the value of the old freq in the second dict
+                frequency[old_f]-=1
+
             if new_f in frequency:
                 # increment the value of the new freq in the second dict if it exists 
                 frequency[new_f]+=1
-                # init the freq in the second dict to 1 if it doesn't
             else:
+                # init the freq in the second dict to 1 if it doesn't
                 frequency[new_f]=1
-            # decrement the value of the old freq in the second dict
-            frequency[old_f]-=1
 
         if q[0] == 2:
-            if q[1] in data: 
+            if q[1] in data and data[q[1]]>0: 
                 # decrement the value of the old freq in the second dict
                 frequency[data[q[1]]]-=1
                 # decrement the data if it exists in the dict
                 data[q[1]]-=1
                 # increment the value of the new freq in the second dict
-                frequency[data[q[1]]]+=1
+                if data[q[1]] in frequency:
+                    frequency[data[q[1]]]+=1
+                else:
+                    frequency[data[q[1]]]=1
             # do nothing if the data isn't in the dict
         if q[0] == 3:
             # check if the second dict has query[1] as a key and if its value > 0
-            if q[1] in frequency:
+            if q[1] in frequency and frequency[q[1]]>0:
                 # add a 1 to our output array
                 output.append(1)
             else:
                 # add a 0 to our output array
                 output.append(0)
     # return our output array
+    print(output)
     return output
 
 if __name__ == '__main__':
-    f = open('data.txt', 'r')
-    q = f.read()
+    f = open('data2.txt', 'r')
     queries = []
-    for _ in range(q):
-        queries.append(list(map(int, input().rstrip().split())))
-    print(queries)
-    # # (1,624),(1,980),(2,18),(2,13),(2,1),(2,3),(1,887),(1,183),(3,2),(2,17),(2,10),(2,1),(2,19),(1,763),(2,20),
-    # ans = freqQuery(queries)
-    # print(ans)
+    q = f.read().split()
+    for x in range(0,len(q),2):
+        first=int(q[x])
+        second=int(q[x+1])
+        queries.append([first,second])
+        # print(queries)
+    ans = freqQuery(queries)
+    numprnt=86
+    # print(ans[:numprnt])
+
+    f.close()
+    f = open('answer2.txt', 'r')   
+    right = f.read().split()
+    right=list(map(int,right))
+    print(ans,ans==right)
