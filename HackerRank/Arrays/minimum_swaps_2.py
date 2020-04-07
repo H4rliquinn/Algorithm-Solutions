ad747e8078817cca95277abce05c64f34a9ddc38
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-07 16:41:57 -0400

    Added Minimum Swaps 2

---
+++ HackerRank/Arrays/minimum_swaps_2.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    swaps=0
+    for x in range(len(arr)):
+        if arr[x]!=x+1:
+            loc=arr.index(x+1)
+            arr[x],arr[loc]=arr[loc],arr[x]
+            swaps+=1
+    print(swaps)
+    return swaps
+
+if __name__ == '__main__':
+    
+
+    
+
+    arr =[1,3,5,2,4,6,7]
+
+    res = minimumSwaps(arr)
+    print(res)
