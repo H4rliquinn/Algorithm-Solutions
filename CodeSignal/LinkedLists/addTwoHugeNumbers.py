fe222302f5bd45eca67c408e228b5fa84288debc
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-08 19:42:17 -0400

    All tests passed

--- CodeSignal/LinkedLists/addTwoHugeNumbers.py
+++ CodeSignal/LinkedLists/addTwoHugeNumbers.py
@@ -9,7 +9,6 @@ def addTwoHugeNumbers(a, b):
     curr_b=b
     str_a=''
     str_b=''
-    
 
     while curr_a is not None:
         val=str(curr_a.value)
@@ -21,4 +20,19 @@ def addTwoHugeNumbers(a, b):
         str_b+=f'{"0"*(4-len(val))}{val}'
         curr_b=curr_b.next
 
-    print(int(str_a),int(str_b))
+    res=str(int(str_a)+int(str_b))
+    resll=None    
+    while len(res)>4:
+        if resll is None:
+            resll=ListNode(int(res[-4:]))
+            res=res[:-4]
+        else:
+            new_node=ListNode(int(res[-4:]))
+            new_node.next=resll
+            resll=new_node
+            res=res[:-4]
+    new_node=ListNode(int(res[-4:]))
+    new_node.next=resll
+    resll=new_node
+
+    return resll
