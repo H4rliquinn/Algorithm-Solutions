c8282683ce35a964b919d4ff578fd009562effff
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-27 19:37:37 -0400

    Finally figured it out. Kept reverting back to False on the way up

--- CodeSignal/Trees/hasPathWithGivenSum.py
+++ CodeSignal/Trees/hasPathWithGivenSum.py
@@ -10,3 +10,25 @@ Given a binary tree t and an integer s, determine whether there is a root to lea
 #     self.left = None
 #     self.right = None
 def hasPathWithGivenSum(t, s):
+    # Return if Empty edge case
+    if t==None:
+        return s==0
+    ret_val=False
+    # Calculate current value remaining
+    new_sum=s-t.value
+
+    # If sum is correct at leaf return True
+    if t.left==None and t.right==None:
+        if new_sum==0:
+            return True
+    # Move down if possible
+    if t.left!=None:
+        new_val=hasPathWithGivenSum(t.left,new_sum)
+        if new_val or ret_val:
+            ret_val=True
+    if t.right!=None:
+        new_val=hasPathWithGivenSum(t.right,new_sum)
+        if new_val or ret_val:
+            ret_val=True
+
+    return ret_val
