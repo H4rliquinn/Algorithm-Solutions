63e1d6de877a5a8b60c4b9c8469c12fcba496b87
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-27 18:32:57 -0400

    Added Has Path With Given Sum

---
+++ CodeSignal/Trees/hasPathWithGivenSum.py
@@ -0,0 +1,12 @@
+"""
+Given a binary tree t and an integer s, determine whether there is a root to leaf path in t such that the sum of vertex values equals s.
+"""
+
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def hasPathWithGivenSum(t, s):
