9f45ba66e2b21507895f444392baac40885c760a
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-28 22:57:38 -0400

    Added Are Following Patterns

---
+++ CodeSignal/Hashtables/areFollowingPatterns.py
@@ -0,0 +1,4 @@
+"""
+Given an array strings, determine whether it follows the sequence given in the patterns array. In other words, there should be no i and j for which strings[i] = strings[j] and patterns[i] ≠ patterns[j] or for which strings[i] ≠ strings[j] and patterns[i] = patterns[j].
+"""
+def areFollowingPatterns(strings, patterns):
