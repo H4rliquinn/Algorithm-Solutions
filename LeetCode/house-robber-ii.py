80e35d1fdede3afdf57f3898db13fcdad8602742
Jason Sonnichsen <Harliquinn@gmail.com>
2020-06-29 20:05:06 -0400

    House robber complete

--- LeetCode/house-robber-ii.py
+++ LeetCode/house-robber-ii.py
@@ -8,4 +8,25 @@ Output: 3
 Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
              because they are adjacent houses.
 '''
-
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0: 
+            return 0
+        if len(nums) == 1: 
+            return nums[0] 
+ 
+        def robber_helper(nums,i,j):
+            if i==j:
+                return nums[i]
+            cache = [0]*len(nums)      
+            cache[i]=nums[i]
+            cache[i+1]=max(nums[i+1],cache[i])
+            
+            for x in range(i+2, j): 
+                cache[x] = max(nums[x]+cache[x-2], cache[x-1])
+            return cache[-1]
+        
+        max1=robber_helper(nums,0,len(nums)-2)
+        max2=robber_helper(nums,1,len(nums)-1)
+         
+        return max(max1,max2)
