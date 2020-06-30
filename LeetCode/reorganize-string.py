7b5274e5734b2ccd80ce3c7da90c93bbc63c86c7
Jason Sonnichsen <Harliquinn@gmail.com>
2020-06-30 13:22:59 -0400

    Reorganize String complete

--- LeetCode/reorganize-string.py
+++ LeetCode/reorganize-string.py
@@ -11,4 +11,52 @@ Example 2:
 Input: S = "aaab"
 Output: ""
 """
-
+class Solution:
+    def reorganizeString(self, S: str) -> str:
+        rtn=""
+        prev_char=""
+        first_char=""
+        hold=[]
+        
+        def useHold(first_char,prev_char,rtn):
+            print(hold,prev_char)
+            if len(hold)>0:
+                rep=hold[0]
+                if rep!=prev_char:
+                    prev_char=hold.pop(0)
+                    rtn+=prev_char
+                elif rep!=first_char:
+                    first_char=hold.pop(0)
+                    rtn=first_char+rtn 
+            return (first_char,prev_char,rtn)
+        
+        for x in S:
+            if first_char=="":
+                first_char=x
+                first_char,prev_char,rtn=useHold(first_char,prev_char,rtn)
+            if x!=prev_char:
+                rtn+=x
+                prev_char=x
+                first_char,prev_char,rtn=useHold(first_char,prev_char,rtn)
+            elif x!=first_char:
+                first_char=x
+                rtn=x+rtn
+            else:
+                hold.append(x)
+        print("HOLD",hold,rtn)
+        while len(hold)>0:
+            cur=hold.pop(0)
+            pChar=""
+            index=0
+            flag=False
+            for c in rtn:
+                if c!=cur and pChar!=cur:
+                    print(rtn[0:index],cur,rtn[index:])
+                    rtn=rtn[0:index]+cur+rtn[index:]
+                    flag=True
+                pChar=c
+                index+=1 
+            if flag==False:
+                return ""
+            
+            return rtn
