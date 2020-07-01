4ad4927f1178b25253ed6ad0703a865d4bdcae22
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-01 17:29:52 -0400

    Got some help, but realized it was similar to steps Working now

--- LeetCode/decode-ways.py
+++ LeetCode/decode-ways.py
@@ -18,37 +18,20 @@ Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 '''
 class Solution:
     def numDecodings(self, s: str) -> int:
-        count=1
-        used=1
-        
         list(s)
         if s[0]=='0':
             return 0
+        count=[0]*len(s)
+        count[0]=1
         
-        for x in range(len(s)):
-            if x>0:
-                
-                
-                if s[x]=='0':
-                    used=-1
-                    if count>1:
-                        count-=1
-                    if used==0 or s[x-1]=='0' or int(s[x-1])>2:
-                        return 0
-                    # else:
-                elif int(s[x-1]+s[x])>26 or s[x-1]=="0":
-                    if used==-1:
-                        used=1
-                    else:
-                        used=1
+        for x in range(1,len(s)):
+            curr=int(s[x])
+            prev=int(s[x-1:x+1])
+            if curr>=1 and curr<=9:
+                count[x]+=count[x-1]
+            if prev>=10 and prev<=26:
+                if x>=2:
+                    count[x]+=count[x-2]
                 else:
-                    if used==0:
-                        used=1
-                        count+=2
-                    elif used==1:
-                        used=0
-                        count+=1
-                    else:
-                        used=1
-            print(used,count)
-        return count
+                    count[x]+=1      
+        return count[-1]
